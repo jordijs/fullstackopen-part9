49397a8b26d64548308fcb9529c0825679c1cd52
Jordi <jordijs6@gmail.com>
2024-08-19 22:40:35 +0200

    9.1 - create calculateBmi function

---
+++ bmiCalculator.ts
@@ -0,0 +1,19 @@
+
+
+const calculateBmi = (height: number, weight: number): string => {
+
+    const bmi = weight / height * height;
+
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if ((bmi >= 18.5) && (bmi < 25)) {
+        return 'Normal range';
+    } else if ((bmi >= 25) && (bmi < 30)) {
+        return 'Overweight';
+    } else {
+        return 'Obese';
+    }
+
+}
+
+console.log(calculateBmi(180, 74));
