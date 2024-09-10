6a10acdb51e3f9d22be9382cd54e7dd69de56e78
Jordi <jordijs6@gmail.com>
2024-09-10 15:38:30 +0200

    9.2 - finish exerciseCalculator

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -10,15 +10,39 @@ interface ObjectExercise {
 
 const calculateExercises = (exercise: number[], target: number): ObjectExercise => {
 
+    const periodLength = exercise.length;
+
+    const trainingDays = exercise.filter(day => day > 0).length;
+
+    const sum = exercise.reduce(
+        (accumulator: number, currentValue: number) => accumulator + currentValue, 0
+    );
+
+    const average = sum / periodLength;
+
+    let rating: number;
+    let ratingDescription: string;
+    let success: boolean;
+
+    if (average >= target) {
+        rating = 3;
+        ratingDescription = 'great! you reached your goal';
+        success = true;
+    } else if (average > (target / 2)) {
+        rating = 2;
+        ratingDescription = 'not too bad but could be better';
+        success = false;
+    } else {
+        rating = 1;
+        ratingDescription = 'too far from your goal, you should train more!';
+        success = false;
+    }
+
     return {
-        periodLength: 7,
-        trainingDays: 5,
-        success: false,
-        rating: 2,
-        ratingDescription: 'not too bad but could be better',
-        target: 2,
-        average: 1.9285714285714286
+        periodLength, trainingDays, success, rating, ratingDescription, target, average
     };
 };
 
-calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2)
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+console.log(calculateExercises([3, 3, 2, 4.5, 5.5, 3, 1, 3, 3, 2, 4.5, 5.5, 3, 1], 2));
+console.log(calculateExercises([1, 0, 1, 0, 1], 3));
