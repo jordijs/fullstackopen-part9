c3666ca7243c49eb00832cc88e4326abb647dafb
Jordi <jordijs6@gmail.com>
2024-09-11 16:03:23 +0200

    9.5 - get query string parameters using query parser

--- index.ts
+++ index.ts
@@ -1,17 +1,22 @@
 import express from 'express';
-import bmiCalculator from './bmiCalculator';
+// import bmiCalculator from './bmiCalculator';
+import qs from 'qs';
 
 const app = express();
+
+app.set('query parser',
+    (str: string) => qs.parse(str, { /* custom options */ }));
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
 });
 
 app.get('/bmi', (req, res) => {
+    console.log(req.query);
     res.send('bmi calculator in progress');
 });
 
-const PORT = 3001;
+const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
